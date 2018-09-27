694af5615a00a6ff25fbe7ecfdc9a7986c92bd22
anxu829 <40457335+anxu829@users.noreply.github.com>
2018-09-27 12:07:19 +0800

    Update start_gluon.sh

--- start_gluon.sh
+++ start_gluon.sh
@@ -2,7 +2,7 @@ container='xuan_gluoncv'
 image='anxu829/gluoncv:withlab'
 sudo docker stop $container 
 sudo docker rm $container
-sudo nvidia-docker run --name $container   -it \
+sudo nvidia-docker run --name $container  -e CUDA_VISIBLE_DEVICES=0  -it \
            -v /data/el-train/TRAIN/mxnet_yinlieClassify:/xuan \
            -v /data/el-train/TRAIN/xuhan_train/:/xuhan \
            -v /data/el-train/:/el-train \
